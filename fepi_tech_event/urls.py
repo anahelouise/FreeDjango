06a31fe73cb2855ce6a05ad0cdcc398ec7e60a00
Ana Helouise <anahelouise.ss@gmail.com>
2024-10-03 21:21:23 -0300

    delete

--- fepi_tech_event/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from poesias.views import root_view, home
-
-urlpatterns = [
-    path('', root_view),
-    path('home/', home),
-]
--- fepi_tech_event/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request, 'index.html')
-
-def root_view(request):
-    return render(request, "index.html")
